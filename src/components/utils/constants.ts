9e08ef4d512b17ebe08013c3c0c76d4b39bf489f
Kavuma Herbert <herberthtk100@gmail.com>
2022-12-09 19:22:05 +0300

    Update constants.ts
    Update url

--- src/components/utils/constants.ts
+++ src/components/utils/constants.ts
@@ -2,6 +2,6 @@ const host = window.location.hostname;
 export const SERVER_URL =
   host === 'localhost'
     ? 'http://localhost:8000'
-    : 'https://cohot2.herokuapp.com';
+    : 'https://finance-manager-backend.onrender.com';
 export const clientId =
   '464607925379-cirq4f7fqka2eskmhhdk9n8d04gu5kbl.apps.googleusercontent.com';
