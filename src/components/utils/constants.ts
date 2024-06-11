d3b096675efcb8fe07cdee985f9eced21fed3b5f
Herbert Kavuma <you@example.com>
2024-06-11 10:24:58 +0300

    Changed base URL

--- src/components/utils/constants.ts
+++ src/components/utils/constants.ts
@@ -2,6 +2,6 @@ const host = window.location.hostname;
 export const SERVER_URL =
   host === 'localhost'
     ? 'http://localhost:8000'
-    : 'https://finance-manager.cyclic.app';
+    : 'https://finance-manager-backend.onrender.com';
 export const clientId =
   '464607925379-cirq4f7fqka2eskmhhdk9n8d04gu5kbl.apps.googleusercontent.com';
